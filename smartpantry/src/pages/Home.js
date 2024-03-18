7c556762dae1a2b1dcf5d798d592c4244f42ba82
Richard Varela <rvarela@csus.edu>
2024-03-18 14:21:50 -0700

    It finally works.

--- smartpantry/src/pages/Home.js
+++ smartpantry/src/pages/Home.js
@@ -4,18 +4,40 @@ import './homeStyleSheet/home.css';
 import bluefoodimage from './homeStyleSheet/bluefoodimage.jpg'; 
 import './moreInfoStyleSheet/moreinfo.css';
 
+
+
+async function grabData(options) {
+  const response = await fetch("http://localhost:8000/api/recipes/getMyRecipes", options);
+  let user_data = await response.json();
+
+  let arr = [];
+  for (let i = 0; i < user_data.length; i++) {
+    arr.push({ title: user_data[i].recipe_name, ingredients: user_data[i].ingredients, time: user_data[i].time });
+  }
+  return arr;
+}
+
+function parseData(recipe_data) {
+  let arr = [];
+
+  for (let i = 0; i < recipe_data.length; i++) {
+    arr.push({ title: recipe_data[i].recipe_name, ingredients: recipe_data[i].ingredients, time: recipe_data[i].time });
+  }
+  return arr;
+}
+
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState(''); // State to hold the search query
   const navigate = useNavigate(); // Hook to programmatically navigate
-
+  const [userResults, setUserResults] = useState();
   //const userName = localstorage.getItem("username");
-
+/*
   const recipes = [
     { title: "Pancakes", ingredients: ["Egg", "Flour", "Vanilla extract"], time: "8 minutes" },
     { title: "Grilled Cheese", ingredients: ["Cheese", "Bread", "Butter"], time: "5 minutes" },
     { title: "Smoothie", ingredients: ["Banana", "Milk", "Honey", "Ice"], time: "5 minutes" },
   ];
-
+*/
   // Testing here
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -30,11 +52,24 @@ const Home = () => {
     body: raw,
     redirect: "follow"
   };
-  
+
   fetch("http://localhost:8000/api/recipes/getMyRecipes", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+  .then((response) => response.text())
+  .then((result) => setUserResults(result))
+  .catch((error) => console.error(error));
+
+  let stuff = JSON.parse(userResults);
+  //console.log("stuff: " + stuff[0].user_name);
+  let recipes = parseData(stuff);
+  //console.log("Does it work?");
+  console.log(recipes);
+
+/*
+  let user_recipe_data = grabData(requestOptions);
+  console.log(user_recipe_data);
+  let recipes = parseData(user_recipe_data);  
+  console.log(recipes);
+*/
   // End testing
 
   const handleSearchInput = (event) => {

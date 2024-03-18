ae01ccc11192e00d890782fcb0af9c2b0b5d511b
AdibaAdi <adiba.akter.98@gmail.com>
2024-03-18 07:44:53 -0700

    completed foodstorage page

--- smartpantry/src/index.js
+++ smartpantry/src/index.js
@@ -8,7 +8,7 @@ import ErrorPage from "./pages/ErrorPage";
 import Login from "./pages/loginUI/Login"
 import Register from "./pages/loginUI/Register"
 import CreateRecipe from "./pages/CreateRecipe"
-import foodstorage from "./pages/foodstorage";
+import Foodstorage from "./pages/Foodstorage";
 
 export default function App() {
   return (
@@ -18,7 +18,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="RecipeSearch" element={<RecipeSearch />} />
           <Route path="Leftovers" element={<Leftovers />} />
-          <Route path="foodstorage" element={<foodstorage />} />
+          <Route path="food-storage-tips" element={<Foodstorage />} />
           <Route path="CreateRecipe" element={<CreateRecipe />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
